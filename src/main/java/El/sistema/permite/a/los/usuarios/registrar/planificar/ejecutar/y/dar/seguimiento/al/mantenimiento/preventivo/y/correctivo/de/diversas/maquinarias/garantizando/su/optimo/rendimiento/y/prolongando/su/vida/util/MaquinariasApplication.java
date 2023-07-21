20a0567d07ab164fc0706f2e2214a933a15307ec
Javier Collao <javier.collao@usach.cl>
2023-07-21 14:18:25 -0400

    Add folders

---
+++ src/main/java/El/sistema/permite/a/los/usuarios/registrar/planificar/ejecutar/y/dar/seguimiento/al/mantenimiento/preventivo/y/correctivo/de/diversas/maquinarias/garantizando/su/optimo/rendimiento/y/prolongando/su/vida/util/MaquinariasApplication.java
@@ -0,0 +1,13 @@
+package El.sistema.permite.a.los.usuarios.registrar.planificar.ejecutar.y.dar.seguimiento.al.mantenimiento.preventivo.y.correctivo.de.diversas.maquinarias.garantizando.su.optimo.rendimiento.y.prolongando.su.vida.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquinariasApplication.class, args);
+	}
+
+}
