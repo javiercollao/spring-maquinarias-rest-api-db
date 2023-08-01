590ef1e9aad5e3b1374337de1110fb74c2196068
Javier Collao <javier.collao@usach.cl>
2023-07-31 23:57:04 -0400

    Update Documentacion entity

--- src/main/java/maquinariasapp/entity/Documentacion.java
+++ src/main/java/maquinariasapp/entity/Documentacion.java
@@ -1,7 +1,11 @@
 package maquinariasapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +22,9 @@ public class Documentacion {
 
     @Column(name="tipo_documentacion", length = 30, nullable = false)
     private String tipo_documentacion;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "maquina_documentacion", joinColumns = @JoinColumn(name = "id_documentacion", referencedColumnName = "id_documentacion"), inverseJoinColumns = @JoinColumn(name = "id_maquinaria", referencedColumnName = "id_maquinaria"))
+    @JsonIgnore
+    private List<Maquinaria> maquinarias = new ArrayList<>();
 }
