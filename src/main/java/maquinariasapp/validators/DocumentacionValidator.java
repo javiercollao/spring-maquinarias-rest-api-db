2985d1c177fbdf6767f885453bf46c511ac5a985
Javier Collao <javier.collao@usach.cl>
2023-08-08 00:15:27 -0400

    Add DocumentacionValidator

--- src/main/java/maquinariasapp/validators/DocumentacionValidator.java
+++ src/main/java/maquinariasapp/validators/DocumentacionValidator.java
@@ -1,29 +1,29 @@
 package maquinariasapp.validators;
 
 import maquinariasapp.entity.Documentacion;
+import maquinariasapp.exceptions.ValidateServiceException;
 
 public class DocumentacionValidator {
     public static void validacionDeGuardado(Documentacion documentacion) {
 
         if (documentacion.getNombre_documentacion() == null || documentacion.getNombre_documentacion().isEmpty()) {
-            throw new RuntimeException("El nombre de la documentación es requerido");
+            throw new ValidateServiceException("El nombre de la documentación es requerido");
         }
 
         if (documentacion.getNombre_documentacion().length() > 70){
-            throw new RuntimeException("El nombre de la documentación es muy largo");
+            throw new ValidateServiceException("El nombre de la documentación es muy largo");
         }
 
         if (documentacion.getDesc_documentacion() == null || documentacion.getDesc_documentacion().isEmpty()) {
-            throw new RuntimeException("La descripción de la documentación que se quiere añadir es requerida");
+            throw new ValidateServiceException("La descripción de la documentación que se quiere añadir es requerida");
         }
 
         if (documentacion.getDesc_documentacion().length() > 200){
-            throw new RuntimeException("La descripción de la documentación es muy larga");
+            throw new ValidateServiceException("La descripción de la documentación es muy larga");
         }
 
         if (documentacion.getArchivo_documentacion() == null || documentacion.getArchivo_documentacion() .isEmpty()) {
-            throw new RuntimeException("El archivo de la documentación es requerido");
+            throw new ValidateServiceException("El archivo de la documentación es requerido");
         }
-
     }
 }
