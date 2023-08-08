e5142410a60201afd1dddf4484017c789738a222
Javier Collao <javier.collao@usach.cl>
2023-08-08 00:15:14 -0400

    Update DocumentacionService

--- src/main/java/maquinariasapp/service/impl/DocumentacionService.java
+++ src/main/java/maquinariasapp/service/impl/DocumentacionService.java
@@ -1,6 +1,10 @@
 package maquinariasapp.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import maquinariasapp.entity.Documentacion;
+import maquinariasapp.exceptions.GeneralServiceException;
+import maquinariasapp.exceptions.NoDataFoundException;
+import maquinariasapp.exceptions.ValidateServiceException;
 import maquinariasapp.repository.DocumentacionRepository;
 import maquinariasapp.services.IDocumentacionService;
 import maquinariasapp.validators.DocumentacionValidator;
@@ -11,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
 
-
+@Slf4j
 @Service
 public class DocumentacionService implements IDocumentacionService {
 
@@ -23,8 +27,12 @@ public class DocumentacionService implements IDocumentacionService {
     public List<Documentacion> obtenerTodasLasDocumentaciones(Pageable page) {
         try{
             return documentacionRepository.findAll(page).toList();
-        }catch (Exception e){
-            return null;
+        } catch (ValidateServiceException | NoDataFoundException e){
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e){
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
         }
     }
 
@@ -32,34 +40,50 @@ public class DocumentacionService implements IDocumentacionService {
     @Transactional(readOnly = true)
     public Documentacion obtenerDocumentacionPorId(Long documentacionId) {
         try {
-            Documentacion resultado = documentacionRepository.findById(documentacionId).orElseThrow();
+            Documentacion resultado = documentacionRepository.findById(documentacionId)
+                    .orElseThrow(()-> new NoDataFoundException("No existe el registro con ese ID."));
             return resultado;
-        }catch (Exception e){
-            return null;
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
         }
     }
 
     @Override
+    @Transactional
     public Documentacion crearNuevaDocumentacion(Documentacion documentacion) {
         try {
             DocumentacionValidator.validacionDeGuardado(documentacion);
             return documentacionRepository.save(documentacion);
-        }catch (Exception e){
-            return null;
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
         }
     }
 
     @Override
+    @Transactional
     public Documentacion actualizarDocumentacion(Long id, Documentacion documentacion) {
         try {
             DocumentacionValidator.validacionDeGuardado(documentacion);
-            Documentacion registro = documentacionRepository.findById(id).orElseThrow();
+            Documentacion registro = documentacionRepository.findById(id)
+                    .orElseThrow(()-> new NoDataFoundException("No existe el registro con ese ID."));
             registro.setNombre_documentacion(documentacion.getNombre_documentacion());
             registro.setDesc_documentacion(documentacion.getDesc_documentacion());
             registro.setArchivo_documentacion(documentacion.getArchivo_documentacion());
             return documentacionRepository.save(registro);
-        }catch (Exception e){
-            return null;
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
         }
     }
 
