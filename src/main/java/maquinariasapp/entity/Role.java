dd9b518b43c0319ad5d9296a1a22d5def3d4c3a5
Javier Collao <javier.collao@usach.cl>
2023-08-15 23:53:59 -0400

    Add Role

---
+++ src/main/java/maquinariasapp/entity/Role.java
@@ -0,0 +1,14 @@
+package maquinariasapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String rol;
+}
